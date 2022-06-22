5f74d66b46a6541f00baffa06574996ecd2dfda9
isra.dev@outlook.com <isra.dev@outlook.com>
2022-06-22 15:11:18 -0500

    Final Commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
+
         //Ejercicio if
         int numeroIf = 85;
         if(numeroIf < 0) {
@@ -10,6 +11,7 @@ public class App {
         } else {
             System.out.println("El número " + numeroIf + " es cero");
         }
+
         // Ejercicio While       
         int numeroWhile = -5;
 
@@ -17,6 +19,7 @@ public class App {
             numeroWhile++;
             System.out.println("Ejercicio While es " + numeroWhile);
         }
+
         // Ejercicio DoWhile
         int numeroDowhile = 3;
 
@@ -24,5 +27,34 @@ public class App {
             System.out.println("Ejercicio DoWhile es " + numeroDowhile);
             numeroDowhile++;
         } while (numeroDowhile == 3);
+
+        // Ejercicio For
+        
+        for (int numeroFor = 0; numeroFor <=3; numeroFor++) {
+            System.out.println("Ejercicio For es " + numeroFor);
+        }
+
+        // Ejercicio Switch
+        var estacion = "Primavera"; 
+        switch (estacion) {
+            case "Primavera":
+            System.out.println("La estación actual es Primavera");
+            break;
+
+            case "Verano":
+            System.out.println("La estación actual es Verano");
+            break;
+
+            case "Otoño":
+            System.out.println("La estación actual es Otoño");
+            break;
+
+            case "Invierno":
+            System.out.println("La estación actual es Invierno");
+            break;
+
+        default:
+            System.out.println("No escogiste una estación listillo");
+        }
     }
 }
