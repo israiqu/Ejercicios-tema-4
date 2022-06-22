a97560794ad76e4bd80a667f564c949c61efd105
isra.dev@outlook.com <isra.dev@outlook.com>
2022-06-22 14:57:35 -0500

    Parcial commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Ejercicio if
+        int numeroIf = 85;
+        if(numeroIf < 0) {
+
+            System.out.println("El número " + numeroIf + " es negativo");
+        } else if(numeroIf > 0) {
+            System.out.println("El número " + numeroIf + " es positivo");
+        } else {
+            System.out.println("El número " + numeroIf + " es cero");
+        }
+        // Ejercicio While       
+        int numeroWhile = -5;
+
+        while (numeroWhile < 3) {
+            numeroWhile++;
+            System.out.println("Ejercicio While es " + numeroWhile);
+        }
+        // Ejercicio DoWhile
+        int numeroDowhile = 3;
+
+        do {
+            System.out.println("Ejercicio DoWhile es " + numeroDowhile);
+            numeroDowhile++;
+        } while (numeroDowhile == 3);
     }
 }
